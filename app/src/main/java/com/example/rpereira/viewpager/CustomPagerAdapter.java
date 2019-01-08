a6a2f6a30978dad6e6d54fd3177bea78a1468d43
Roger Philippe Pereira <rogerphilippe@outlook.com>
2019-01-08 16:06:28 -0200

    Initial commit

---
+++ app/src/main/java/com/example/rpereira/viewpager/CustomPagerAdapter.java
@@ -0,0 +1,51 @@
+package com.example.rpereira.viewpager;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class CustomPagerAdapter extends PagerAdapter {
+
+    private Context mContext;
+
+    public CustomPagerAdapter(Context context) {
+        mContext = context;
+    }
+
+    @Override
+    public int getCount() {
+        return ModelObject.values().length;
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return view == object;
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+        ModelObject modelObject = ModelObject.values()[position];
+        LayoutInflater inflater = LayoutInflater.from(mContext);
+        ViewGroup layout = (ViewGroup) inflater.inflate(modelObject.getLayoutResId(), container ,
+                false);
+        container.addView(layout);
+        return layout;
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        container.removeView((View) object);
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        ModelObject customPagerEnum = ModelObject.values()[position];
+        return mContext.getString(customPagerEnum.getTitleResId());
+    }
+}
---
+++ app/src/main/java/com/example/rpereira/viewpager/ModelObject.java
@@ -0,0 +1,24 @@
+package com.example.rpereira.viewpager;
+
+public enum ModelObject {
+
+    LEFT(R.string.layout_esquerda, R.layout.left_layout),
+    CENTER(R.string.layout_meio, R.layout.center_layout),
+    RIGHT(R.string.layout_direita, R.layout.right_layout);
+
+    private int mTitleResId;
+    private int mLayoutResId;
+
+    ModelObject(int titleResId, int layoutResId) {
+        mTitleResId = titleResId;
+        mLayoutResId = layoutResId;
+    }
+
+    public int getTitleResId() {
+        return mTitleResId;
+    }
+
+    public int getLayoutResId() {
+        return mLayoutResId;
+    }
+}
